8f604e3e60ad914ff31cc3e3f16ba563e7a0feb3
Travis Deshotels <travis.deshotels@gmail.com>
2019-11-24 16:24:07 -0600

    Initial commit

---
+++ Copywriter/copywriter.py
@@ -0,0 +1,49 @@
+import re as regex
+import sys
+
+"""
+Replace the copyright header in a sourcecode file with a new one using a template
+A file named 'template' must be defined in the same directory as this file
+Usage: python copywriter.py <file>
+"""
+
+def fill_template(template_data, file_name):
+    #add more template variables as needed
+    filename_parts = regex.split("\/", file_name)
+    short_filename = regex.sub("\.[a-zA-Z]+", "", filename_parts[-1])
+    return regex.sub("\{FILENAME\}", short_filename, template_data)
+
+def load_copyright_template(file_name):
+    with open('template', 'r') as infile:
+        template_data = infile.read()
+    return fill_template(template_data, file_name)
+
+def load_file_data(file_name):
+    with open(file_name, 'r') as infile:
+        file_data = infile.read()
+    return file_data
+
+def strip_copyright_block(file_data):
+    #search for "Copyright" and "all rights reserved"
+    #ignores case
+    return regex.sub("\/\*[^/]*[Cc]opyright[\S\s]*[Aa]ll\s[Rr]ights\s[Rr]eserved[^/]*\*\/", "", file_data)
+
+def write_output(output_data, output_file):
+    with open(output_file, 'w') as outfile:
+        outfile.write(output_data)
+
+def validate_arg():
+    if len(sys.argv) == 1:
+        print("Need a filename!")
+        sys.exit()
+
+def main():
+    validate_arg()
+    file_name = sys.argv[1]
+    template_data = load_copyright_template(file_name)
+    file_data = load_file_data(file_name)
+    stripped_data = strip_copyright_block(file_data)
+    write_output(template_data + stripped_data, file_name)
+
+if __name__ == "__main__":
+    main()
