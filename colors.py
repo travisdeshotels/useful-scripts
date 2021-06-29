9afdfb51744b9adb36e54205490457f3984a2310
Travis Deshotels <travis.deshotels@gmail.com>
2021-06-28 19:48:41 -0500

    Add iteration script

---
+++ colors.py
@@ -0,0 +1,18 @@
+class Colors:
+    HEADER = '\033[95m'
+    OK_BLUE = '\033[94m'
+    OK_CYAN = '\033[96m'
+    OK_GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END_C = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def green(string):
+    return Colors.OK_GREEN + string + Colors.END_C
+
+
+def red(string):
+    return Colors.FAIL + string + Colors.END_C
---
+++ iteration.py
@@ -0,0 +1,21 @@
+from datetime import date
+from colors import red, green
+
+iteration_start = date(2021, 6, 16)
+iteration_end = date(2021, 9, 22)
+iteration_diff = iteration_end - iteration_start
+number_of_sprints = int(iteration_diff.days / 14)
+days_into_iteration = int((date.today() - iteration_start).days)
+current_sprint = int((days_into_iteration / 14) + 1)
+days_into_sprint = int((days_into_iteration % 14) + 1)
+
+
+def main():
+    print(green('+' * current_sprint) + red('-' * (number_of_sprints - current_sprint)))
+    print(f'Sprint {current_sprint}')
+    print(green('+' * days_into_sprint) + red('-' * (14 - days_into_sprint)))
+    print(f'Day {days_into_sprint}')
+
+
+if __name__ == '__main__':
+    main()
