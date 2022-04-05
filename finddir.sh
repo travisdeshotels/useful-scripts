91fd551f12bae4a555ca6198d19016bc70b5bef6
Travis Deshotels <travis.deshotels@gmail.com>
2022-04-04 19:37:25 -0500

    Select directory when matching more than one

--- finddir.sh
+++ finddir.sh
@@ -1,13 +1,24 @@
 #add this function to your bashrc and create an alias
 
+# shellcheck disable=SC2010
 function finddir {
     PROJECT_DIR=""
 
     match_count=$(ls "$PROJECT_DIR" | grep -ic "$1")
     if [[ $match_count != 1 ]];then
-        echo "ERROR did not match one directory:"
-        ls "$PROJECT_DIR" | grep -i "$1"
+        lines=$(ls "$PROJECT_DIR" | grep -i "$1")
+        i=1
+        lines_array=()
+        while IFS= read -r line
+        do
+            echo $i. "$line"
+            lines_array[$i]="$line"
+            i=$((i+1))
+        done <<< "$lines"
+        echo -n "Select one: "
+        read -r choice
+        cd "$PROJECT_DIR/${lines_array[$choice]}" || exit
     else
-        cd $PROJECT_DIR/$(ls "$PROJECT_DIR" | grep -i "$1")
+        cd "$PROJECT_DIR/$(ls "$PROJECT_DIR" | grep -i "$1")" || exit
     fi
 }
