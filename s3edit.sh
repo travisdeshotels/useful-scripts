1e374b43f6fbf661eab7457f0e35c04fd3085b22
Travis Deshotels <travis.deshotels@gmail.com>
2023-10-02 20:59:52 -0500

    Update s3edit.sh

--- s3edit.sh
+++ s3edit.sh
@@ -3,14 +3,19 @@ set -e
 TEMP_FOLDER=""
 EDITOR=""
 
+if [[ "$#" != 1 ]]; then
+  echo "Error: missing S3 URI."
+  exit 1
+fi
+
 s3uri="$1"
 filename=$(echo "$s3uri" | awk -F "/" '{print $NF}')
 
-aws s3 cp s3uri "$TEMP_FOLDER"
+aws s3 cp "$s3uri" "$TEMP_FOLDER"
 cp "$TEMP_FOLDER/$filename" "$TEMP_FOLDER/${filename}.bak"
 "$EDITOR" "$TEMP_FOLDER/${filename}.bak" "$TEMP_FOLDER/$filename"
 echo -n "Upload changes? y/n: "
 read -r choice
-if [[ "$choice" == "y" ]];then
-  aws s3 cp "$TEMP_FOLDER/$filename" s3uri
+if [[ "$choice" == "y" ]]; then
+  aws s3 cp "$TEMP_FOLDER/$filename" "$s3uri"
 fi
